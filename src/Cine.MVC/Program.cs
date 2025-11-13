997b36ee3e5566c5f632cf8302beb1e57a990c1d
Aylen-xd <nerea06toledo@gmail.com>
2025-11-13 11:02:24 -0300

    Pelicula_agregado
    Tira error en el Alta en el Upsert y en el boton de modificar

--- src/Cine.MVC/Program.cs
+++ src/Cine.MVC/Program.cs
@@ -20,7 +20,7 @@ builder.Services.AddTransient<IRepoEstudio, RepoEstudio>();
 builder.Services.AddTransient<IRepoProduccion, RepoProduccion>();
 builder.Services.AddTransient<IRepoSaga, RepoSaga>();
 builder.Services.AddTransient<IRepoActor, RepoActor>();
-//builder.Services.AddTransient<IRepoPelicula, RepoPelicula>();
+builder.Services.AddTransient<IRepoPelicula, RepoPelicula>();
 
 var app = builder.Build();
 
